cc740187d0c62b19938eeb3e365b01e9e0555167
pkyou <471162774@qq.com>
2019-05-12 13:28:12 +0800

    add solution

---
+++ FibonacciBusiness/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace FibonacciBusiness
+{
+    public class Class1
+    {
+    }
+}
---
+++ FibonacciTest/FibonacciTestClass.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace FibonacciTest
+{
+    public class FibonacciTestClass
+    {
+        [Fact]
+        public void ShouldReturn1WhenGetFirstItem()
+        {
+            
+        }
+    }
+}
