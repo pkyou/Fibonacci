cd4ff0190078cc03e3a24b75408239a1e44004fc
pkyou <471162774@qq.com>
2019-05-12 15:50:15 +0800

    add function: supported:upper index 140

--- FibonacciBusiness/Fibonacci.cs
+++ FibonacciBusiness/Fibonacci.cs
@@ -4,9 +4,35 @@ namespace FibonacciBusiness
 {
     public class Fibonacci
     {
-        public int GetItemByIndex(int i)
+        public static decimal GetItemByIndex(int i)
         {
-            throw new NotImplementedException();
+            if (i>=140)
+            {
+                return 0;
+            }
+            
+            if (i <= 0)
+            {
+                return 0;
+            }
+            
+            if (i == 1 || i == 2)
+            {
+                return 1;
+            }
+
+            decimal preItem = 1;
+            decimal prePreItem = 1;
+            decimal temp = 0;
+            
+            for (int j = 2; j < i; j++)
+            {
+                temp = prePreItem + preItem;
+                prePreItem = preItem;
+                preItem = temp;
+            }
+            
+            return temp;
         }
     }
 }
--- FibonacciTest/FibonacciTestClass.cs
+++ FibonacciTest/FibonacciTestClass.cs
@@ -11,7 +11,26 @@ namespace FibonacciTest
         [Fact]
         public void ShouldReturn1WhenGetFirstItem()
         {
-            Assert.Equal(1,_fibonacci.GetItemByIndex(1));
+            Assert.Equal(1,Fibonacci.GetItemByIndex(1));
         }
+
+        [Fact]
+        public void ShouldReturn1WhenGetSecondItem()
+        {
+            Assert.Equal(1,Fibonacci.GetItemByIndex(2));
+        }
+ 
+        [Fact]
+        public void ShouldReturnSumOfFirstAndSecWhenGetThirdItem()
+        {
+            Assert.Equal(Fibonacci.GetItemByIndex(1)+Fibonacci.GetItemByIndex(2),Fibonacci.GetItemByIndex(3));
+        }
+        
+        [Fact]
+        public void ShouldReturn34WhenGetNinethItem()
+        {
+            Assert.Equal(34,Fibonacci.GetItemByIndex(9));
+        }
+        
     }
 }
